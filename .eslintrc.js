75e908325f4c2d864662098c4c672b821df0f448
Marcelo Mattos <marcelo.mattos@BONAPARTE.LAN>
2019-05-08 17:48:11 -0300

    :rotating_light: Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,17 @@ module.exports = {
   extends: [
     "airbnb-base",
   ],
+  plugins: [
+    'jest'
+  ],
+  env: {
+    "jest/globals": true
+  },
   // add your custom rules here
   rules: {
+    semi: 0,
+    'comma-dangle': 0,
+    'object-curly-newline': 0,
+    'arrow-parens': ['error', 'as-needed']
   },
 };
